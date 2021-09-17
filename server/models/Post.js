c64fab6ce5e9d452ce9ab830e294b1aae0f87ac3
DariusDeah <dariusdeah21@gmail.com>
2021-09-17 14:24:41 -0600

    updated post schema

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 export const PostSchema = new Schema({
-  title: { type: String, minlength: 3, require: true },
-  img: { type: String, required: true }
-  id
+  title: { type: String, minlength: 3, require: true, trim: true },
+  img: { type: String, required: true },
+  description: { type: String },
+  creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
 })
